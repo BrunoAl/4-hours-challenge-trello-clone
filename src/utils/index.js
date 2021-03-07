0587ef3a453e30a50693c691280b2af5aa71868a
Bruno <brunoalnascimento@gmail.com>
2021-03-07 13:44:52 +0100

    docs

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
 /**
  * Removes item from a list based on the id.
- * @param {Object[]} list - list of object with a unique `id` key.
- * @param {*} itemId - `id` of the item to be remove
+ * @param {Object[]} list - list of objects.
+ * @param {string} list[].id - unique id.
+ * @param {*} itemId - `id` of the item to be remove.
+ * @returns {Object[]} - updated list.
  */
 export function removeItemById(list, itemId) {
   return list.filter(item => item.id !== itemId);
